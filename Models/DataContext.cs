2faf30796f7f44fb70f4ac2b2112e332843bb5ed
Alexandre Provensi <alexandreprovensi@gmail.com>
2021-08-04 13:35:25 -0300

    Adicionar arquivos de projeto.

---
+++ Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using CadTruck.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CedTruck
+{
+    internal class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Truck> trucks { get; set; }
+    }
+}
---
+++ Models/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadTruck.Models
+{
+    public enum Model
+    {
+        FH,
+        FM
+    }
+    public class Truck
+    {
+        [Key]
+        public long id { get; set; }
+        public Model model { get; set; }
+        public DateTime yearFabrication { get; set; }
+        public DateTime yearModel { get; set; }
+
+    }
+}
